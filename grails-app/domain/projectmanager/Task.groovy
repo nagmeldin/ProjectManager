651b560abca5a39ac1cac9beff06bac9c050ebb9
nagmeldin <eldin.nagm@gmail.com>
2014-05-25 19:53:17 -0600

    modified

--- grails-app/domain/projectmanager/Task.groovy
+++ grails-app/domain/projectmanager/Task.groovy
@@ -3,6 +3,7 @@ package projectmanager
 class Task 
 {
 	// Model Attributes:
+	
 	String name
 	String toString ()  { "${name}"  } // cleans display of model name in stead of its id
 	String description
