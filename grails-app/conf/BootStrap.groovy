eef865445c11d739652a6adc77f7a23f6407a537
nagmeldin <eldin.nagm@gmail.com>
2014-06-15 22:11:26 -0600

    added

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,8 +2,8 @@ class BootStrap {
 
     def init = { servletContext ->
 		
-		       new projectmanager.EndUser(firstName: "joe", lastName: "doe", userName:"jdoe" , password: "password").save()  //server restart
-		       new projectmanager.EndUser(firstName: "anne", lastName: "doe", userName:"adoe" , password: "password").save()  //server restart
+		       new projectmanager.EndUser(firstName: "Joe", lastName: "Doe", userName:"jdoe" , password: "password").save()  //server restart
+		       new projectmanager.EndUser(firstName: "Anne", lastName: "Doe", userName:"adoe" , password: "password").save()  //server restart
 		
 		    
                 }
