c3e278aeeb21227832af776ba8fcffcbaf8783fe
nagmeldin <eldin.nagm@gmail.com>
2015-06-24 14:38:48 -0600

    PM domain structure change

--- grails-app/domain/projectmanager/Project.groovy
+++ grails-app/domain/projectmanager/Project.groovy
@@ -6,9 +6,9 @@ class Project
 	// Model Attributes:
 	String name
 	String toString ()  { "${name}"  }  // cleans display of model name in stead of its id
-	String description
 	Date dueDate
 	String billType
+	String description 
   
    // Entity Relationships:
 	 static belongsTo = [owner:EndUser]
@@ -19,9 +19,10 @@ class Project
 		 
 		 name blank:false,unique:true  // or alternatively:
 		 //name(blank:false,unique:true)
-		 description()
 		 dueDate(min: new Date())  // has to be future date
 		 billType(inList:["hourly", "daily", "monthly"] )
+		 description(nullable:true)
 	 }
+	 
     
 }
